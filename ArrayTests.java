3a8e6e059b2de160610db4863d6162b965e5a53f
Harry <harry@HarrydeMacBook-Pro.local>
2024-04-23 12:15:47 -0700

    For the reverseInPlace method, I changed the way how it reverses in the for loop by adding a field named “temp” to store the old value at that index. For the reversed method, I changed the assignment order: should assign the value in the old array to the new one.

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,9 +4,13 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = {3};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{3}, input1);
+
+    int[] input2 = {0, 1, 2, 3};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{3, 2, 1, 0}, input2);
 	}
 
 
@@ -14,5 +18,7 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input2 = {0, 1, 2, 3};
+    assertArrayEquals(new int[]{3, 2, 1, 0}, ArrayExamples.reversed(input2));
   }
 }
